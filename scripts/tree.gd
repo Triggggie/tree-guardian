3fbd8d4e74ddcb9197f410d7d05e4b3626565dba
Triggggie <fanny.blaha@gmail.com>
2026-07-26 20:45:23 +0200

    Add tree idle animation
    Add tree idle animation

--- scripts/tree.gd
+++ scripts/tree.gd
@@ -1,6 +1,28 @@
 extends Node2D
 
 
+@export_range(0.0, 0.1, 0.001)
+var idle_scale_amount: float = 0.015
+
+@export_range(0.1, 5.0, 0.1)
+var idle_speed: float = 1.2
+
+var idle_time: float = 0.0
+
+
+func _process(delta: float) -> void:
+	idle_time += delta * idle_speed
+
+	var breath: float = sin(idle_time) * idle_scale_amount
+
+	scale = Vector2(
+		1.0 + breath,
+		1.0 - breath * 0.35
+	)
+
+	queue_redraw()
+
+
 func _draw() -> void:
 	draw_rect(
 		Rect2(-45, -180, 90, 180),
