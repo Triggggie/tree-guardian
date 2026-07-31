7ab439138bbc4a8def911d1dd411789154174216
Triggggie <fanny.blaha@gmail.com>
2026-07-31 09:23:59 +0200

    Finalize run modifier IDs

--- scripts/services/run_modifier_ids.gd
+++ scripts/services/run_modifier_ids.gd
@@ -2,7 +2,21 @@ class_name RunModifierIds
 extends RefCounted
 
 
-const BRANCH_POWER: StringName = &"branch_power"
-const ACTION_SPEED: StringName = &"action_speed"
-const MAX_HEALTH: StringName = &"max_health"
+# Finální obecné modifiery.
+const BRANCH_DAMAGE: StringName = &"branch_damage"
+const ATTACK_SPEED: StringName = &"attack_speed"
+
+const TREE_MAX_HEALTH: StringName = &"tree_max_health"
+const TREE_REGEN_RATE: StringName = &"tree_regen_rate"
+
 const ESSENCE_GAIN: StringName = &"essence_gain"
+
+const HEALING_POWER: StringName = &"healing_power"
+
+
+# Dočasné migrační aliasy.
+# Odstraníme je, jakmile převedeme současné skripty
+# na nové definitivní názvy.
+const BRANCH_POWER: StringName = BRANCH_DAMAGE
+const ACTION_SPEED: StringName = ATTACK_SPEED
+const MAX_HEALTH: StringName = TREE_MAX_HEALTH
