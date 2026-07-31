f6e74fa6b3e98c518c51f06ff1a2fc00b9e95903
Triggggie <fanny.blaha@gmail.com>
2026-07-31 08:24:54 +0200

    Add run modifier IDs

---
+++ scripts/services/run_modifier_ids.gd
@@ -0,0 +1,8 @@
+class_name RunModifierIds
+extends RefCounted
+
+
+const BRANCH_POWER: StringName = &"branch_power"
+const ACTION_SPEED: StringName = &"action_speed"
+const MAX_HEALTH: StringName = &"max_health"
+const ESSENCE_GAIN: StringName = &"essence_gain"
