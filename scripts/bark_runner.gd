c23ed98de2aa15c2db78f1538136fb2c9f07f1b4
Triggggie <fanny.blaha@gmail.com>
2026-08-18 11:22:50 +0200

    Add Bark Runner production visual

--- scripts/bark_runner.gd
+++ scripts/bark_runner.gd
@@ -1,69 +1,25 @@
 extends "res://scripts/bark_beetle.gd"
 
 
-func _draw() -> void:
-	var outline_color := Color("6a341d")
-	var accent_color := Color("c97936")
-	var highlight_color := Color("e2a05c")
-	var body_points := PackedVector2Array([
-		Vector2(-11.0, -18.0),
-		Vector2(11.0, -18.0),
-		Vector2(15.0, 8.0),
-		Vector2(8.0, 22.0),
-		Vector2(-8.0, 22.0),
-		Vector2(-15.0, 8.0),
-	])
-	var outline_points := PackedVector2Array(body_points)
-	outline_points.append(body_points[0])
+@onready var runner_sprite: Sprite2D = $Visual/BarkRunnerSprite
 
-	draw_colored_polygon(
-		body_points,
-		accent_color
+
+func setup_crowd_formation(
+	new_formation_side: float,
+	new_lane_index: int,
+	new_lane_y: float,
+	new_queue_order: int,
+	new_speed_multiplier: float,
+	new_depth_jitter: float,
+	new_scale_multiplier: float
+) -> void:
+	super.setup_crowd_formation(
+		new_formation_side,
+		new_lane_index,
+		new_lane_y,
+		new_queue_order,
+		new_speed_multiplier,
+		new_depth_jitter,
+		new_scale_multiplier
 	)
-	draw_polyline(
-		outline_points,
-		outline_color,
-		3.0,
-		true
-	)
-	draw_circle(
-		Vector2(0.0, -17.0),
-		9.0,
-		highlight_color
-	)
-	draw_line(
-		Vector2(-6.0, -22.0),
-		Vector2(-13.0, -32.0),
-		outline_color,
-		3.0
-	)
-	draw_line(
-		Vector2(6.0, -22.0),
-		Vector2(13.0, -32.0),
-		outline_color,
-		3.0
-	)
-	draw_line(
-		Vector2(-12.0, -3.0),
-		Vector2(-21.0, -8.0),
-		outline_color,
-		3.0
-	)
-	draw_line(
-		Vector2(12.0, -3.0),
-		Vector2(21.0, -8.0),
-		outline_color,
-		3.0
-	)
-	draw_line(
-		Vector2(-12.0, 10.0),
-		Vector2(-20.0, 17.0),
-		outline_color,
-		3.0
-	)
-	draw_line(
-		Vector2(12.0, 10.0),
-		Vector2(20.0, 17.0),
-		outline_color,
-		3.0
-	)
+	runner_sprite.flip_h = new_formation_side > 0.0
