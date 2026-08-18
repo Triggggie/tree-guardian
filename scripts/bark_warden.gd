d95813cba11b5dbddadc1163a9e658abccf81504
Triggggie <fanny.blaha@gmail.com>
2026-08-18 12:45:52 +0200

    Add Bark Warden production visual

--- scripts/bark_warden.gd
+++ scripts/bark_warden.gd
@@ -1,22 +1,25 @@
 extends "res://scripts/bark_beetle.gd"
 
 
-func _draw() -> void:
-	var body_color := Color("34482f")
-	var outline_color := Color("1d281b")
-	var accent_color := Color("78905f")
-	var body_points := PackedVector2Array([
-		Vector2(-42.0, -35.0),
-		Vector2(42.0, -35.0),
-		Vector2(50.0, 24.0),
-		Vector2(28.0, 48.0),
-		Vector2(-28.0, 48.0),
-		Vector2(-50.0, 24.0)
-	])
-	var outline_points := PackedVector2Array(body_points)
-	outline_points.append(body_points[0])
-	draw_colored_polygon(body_points, body_color)
-	draw_polyline(outline_points, outline_color, 5.0, true)
-	draw_circle(Vector2.ZERO, 18.0, accent_color)
-	draw_line(Vector2(-28.0, -34.0), Vector2(-42.0, -55.0), outline_color, 7.0)
-	draw_line(Vector2(28.0, -34.0), Vector2(42.0, -55.0), outline_color, 7.0)
+@onready var warden_sprite: Sprite2D = $Visual/BarkWardenSprite
+
+
+func setup_crowd_formation(
+	new_formation_side: float,
+	new_lane_index: int,
+	new_lane_y: float,
+	new_queue_order: int,
+	new_speed_multiplier: float,
+	new_depth_jitter: float,
+	new_scale_multiplier: float
+) -> void:
+	super.setup_crowd_formation(
+		new_formation_side,
+		new_lane_index,
+		new_lane_y,
+		new_queue_order,
+		new_speed_multiplier,
+		new_depth_jitter,
+		new_scale_multiplier
+	)
+	warden_sprite.flip_h = new_formation_side > 0.0
